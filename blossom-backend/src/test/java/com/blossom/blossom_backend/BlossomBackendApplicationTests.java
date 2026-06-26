407cebe8c327424a1b97add27f62b9fcd20aeee7
BUSHINIMURALIDHARAN <bushinimuralidharan@gmail.com>
2026-06-26 16:24:56 +0530

    added backend project

---
+++ blossom-backend/src/test/java/com/blossom/blossom_backend/BlossomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blossom.blossom_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlossomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
